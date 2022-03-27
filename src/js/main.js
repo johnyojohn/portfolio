1cccab760e98f3ecb6038958e099171ae9c85775
johnyojohn <johanjohnsjo@gmail.com>
2022-03-26 20:48:51 -0400

    need fixes
    :(

--- src/js/main.js
+++ src/js/main.js
@@ -15,4 +15,20 @@ function moveToAboutMe(){
 
   document.getElementById("about_me_id").scrollIntoView({behavior: "smooth",  inline: "nearest"});
 }
+document.getElementById("my_works_header").addEventListener("click", moveToAboutMe);
+function moveToAboutMe(){
+
+  document.getElementById("my_works_id").scrollIntoView({behavior: "smooth",  inline: "nearest"});
+}
+document.getElementById("awards/titles_header").addEventListener("click", moveToAboutMe);
+function moveToAboutMe(){
+
+  document.getElementById("awards/titles_id").scrollIntoView({behavior: "smooth",  inline: "nearest"});
+}
+document.getElementById("contact_header").addEventListener("click", moveToAboutMe);
+function moveToAboutMe(){
+
+  //document.getElementById("about_me_id").scrollIntoView({behavior: "smooth",  inline: "nearest"});
+}
+
 
