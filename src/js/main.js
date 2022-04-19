a8f7bd5b027405124dc8947668c76528b7e9fabf
Jihoon Kang <jihoonkang0829@gmail.com>
2022-04-18 19:45:18 -0500

    styling & navbar debug

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,29 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = () => { stickyNavBar() };
 
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
 
-function myFunction() {
+function stickyNavBar() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
   }
 }
-document.getElementById("learn_about_me_id").addEventListener("click", moveToAboutMe);
-function moveToAboutMe(){
 
-  document.getElementById("about_me_id").scrollIntoView({behavior: "smooth",  inline: "nearest"});
+var homeButton = document.getElementById("home_header");
+var aboutButton = document.getElementById("about_me_header");
+var myWorksButton = document.getElementById("my_works_header");
+var awardsButton = document.getElementById("awards/titles_header");
+var contactButton = document.getElementById("contact_header");
+var learnAboutMeButton = document.getElementById("learn_about_me_id");
+
+homeButton.addEventListener("click", () => smoothScroll("home"));
+aboutButton.addEventListener("click", () => smoothScroll("about_me_id"));
+myWorksButton.addEventListener("click", () => smoothScroll("my_works_id"));
+awardsButton.addEventListener("click", () => smoothScroll("awards/titles_id"));
+learnAboutMeButton.addEventListener("click", () => smoothScroll("about_me_id"));
+
+const smoothScroll = (element) => {
+  document.getElementById(element).scrollIntoView({ behavior: 'smooth' });
 }
-document.getElementById("my_works_header").addEventListener("click", moveToAboutMe);
-function moveToAboutMe(){
-
-  document.getElementById("my_works_id").scrollIntoView({behavior: "smooth",  inline: "nearest"});
-}
-document.getElementById("awards/titles_header").addEventListener("click", moveToAboutMe);
-function moveToAboutMe(){
-
-  document.getElementById("awards/titles_id").scrollIntoView({behavior: "smooth",  inline: "nearest"});
-}
-document.getElementById("contact_header").addEventListener("click", moveToAboutMe);
-function moveToAboutMe(){
-
-  //document.getElementById("about_me_id").scrollIntoView({behavior: "smooth",  inline: "nearest"});
-}
-
-
